33869bb2965c530df9b1d35117e54a30ed624a24
VladislavHolets <vladislav.holets@gmail.com>
2020-04-05 13:49:15 +0300

    initial commit

---
+++ lib/connect_page.dart
@@ -0,0 +1,313 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:roboremote/controll_page.dart';
+
+import 'mqtt/MQTTManager.dart';
+import 'mqtt/state/MQTTAppState.dart';
+
+class Connect extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ConnectState();
+  }
+}
+
+class Params {
+  MQTTAppState appState;
+  MQTTManager manager;
+
+  Params(this.appState, this.manager);
+}
+
+class _ConnectState extends State<Connect> {
+  final TextEditingController _hostTextController = TextEditingController();
+
+  // final TextEditingController _portTextController = TextEditingController();
+  final TextEditingController _messageTextController = TextEditingController();
+  final TextEditingController _topicTextController = TextEditingController();
+  final TextEditingController _userTextController = TextEditingController();
+  final TextEditingController _passwordTextController = TextEditingController();
+  MQTTAppState currentAppState;
+  MQTTManager manager;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /*
+    _hostTextController.addListener(_printLatestValue);
+    _messageTextController.addListener(_printLatestValue);
+    _topicTextController.addListener(_printLatestValue);
+
+     */
+  }
+
+  @override
+  void dispose() {
+    _hostTextController.dispose();
+    //  _portTextController.dispose();
+    _messageTextController.dispose();
+    _topicTextController.dispose();
+    _userTextController.dispose();
+    _passwordTextController.dispose();
+    super.dispose();
+  }
+
+  /*
+  _printLatestValue() {
+    print("Second text field: ${_hostTextController.text}");
+    print("Second text field: ${_messageTextController.text}");
+    print("Second text field: ${_topicTextController.text}");
+  }
+
+   */
+
+  @override
+  Widget build(BuildContext context) {
+    final MQTTAppState appState = Provider.of<MQTTAppState>(context);
+    // Keep a reference to the app state.
+    currentAppState = appState;
+    final Scaffold scaffold =
+        Scaffold(appBar: _buildAppBar(context), body: _buildColumn());
+    return scaffold;
+  }
+
+  Widget _buildAppBar(BuildContext context) {
+    return AppBar(
+      title: const Text('MQTT'),
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+
+  Widget _buildColumn() {
+    return ListView(
+      shrinkWrap: true,
+      padding: EdgeInsets.all(15.0),
+      children: <Widget>[
+        _buildConnectionStateText(
+            _prepareStateMessageFrom(currentAppState.getAppConnectionState)),
+        _buildEditableColumn(),
+        _buildScrollableTextWith(currentAppState.getHistoryText)
+      ],
+    );
+  }
+
+  Widget _buildEditableColumn() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          _buildTextFieldWith(_hostTextController, 'Enter broker address',
+              currentAppState.getAppConnectionState),
+          const SizedBox(height: 10),
+//          _buildTextFieldWith(_portTextController, 'Enter broker port',currentAppState.getAppConnectionState),
+//          const SizedBox(height: 10),
+          _buildTextFieldWith(_userTextController, 'Enter username',
+              currentAppState.getAppConnectionState),
+          const SizedBox(height: 10),
+          _buildTextFieldWith(_passwordTextController, 'Enter password',
+              currentAppState.getAppConnectionState),
+          const SizedBox(height: 10),
+          _buildTextFieldWith(
+              _topicTextController,
+              'Enter a topic to subscribe or listen',
+              currentAppState.getAppConnectionState),
+          const SizedBox(height: 10),
+          _buildPublishMessageRow(),
+          const SizedBox(height: 10),
+          _buildConnectedButtonFrom(currentAppState.getAppConnectionState),
+          _builControllButtonFrom(currentAppState.getAppConnectionState)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPublishMessageRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          child: _buildTextFieldWith(_messageTextController, 'Enter a message',
+              currentAppState.getAppConnectionState),
+        ),
+        _buildSendButtonFrom(currentAppState.getAppConnectionState)
+      ],
+    );
+  }
+
+  Widget _buildConnectionStateText(String status) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+              color: Colors.deepOrangeAccent,
+              child: Text(status, textAlign: TextAlign.center)),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTextFieldWith(TextEditingController controller, String hintText,
+      MQTTAppConnectionState state) {
+    bool shouldEnable = false;
+    bool shouldObscure = false;
+    TextInputType type = TextInputType.text;
+    if (controller == _messageTextController &&
+        state == MQTTAppConnectionState.connected) {
+      shouldEnable = true;
+    } else if ((controller == _hostTextController &&
+            state == MQTTAppConnectionState.disconnected) ||
+        (controller == _topicTextController &&
+            state == MQTTAppConnectionState.disconnected) ||
+        (controller == _userTextController &&
+            state == MQTTAppConnectionState.disconnected)) {
+      shouldEnable = true;
+    } else if ((controller == _passwordTextController &&
+        state == MQTTAppConnectionState.disconnected)) {
+      shouldEnable = true;
+      shouldObscure = true;
+      //type=TextInputType.number;
+    }
+    if ((controller == _passwordTextController)) {
+      shouldObscure = true;
+    }
+    return TextField(
+        enabled: shouldEnable,
+        obscureText: shouldObscure,
+        keyboardType: type,
+        controller: controller,
+        decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.only(left: 0, bottom: 0, top: 0, right: 0),
+          labelText: hintText,
+        ));
+  }
+
+  Widget _buildScrollableTextWith(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blue,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        width: 400,
+        height: 300,
+        child: SingleChildScrollView(
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildConnectedButtonFrom(MQTTAppConnectionState state) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: RaisedButton(
+            color: Colors.lightBlueAccent,
+            child: const Text('Connect'),
+            onPressed: state == MQTTAppConnectionState.disconnected
+                ? _configureAndConnect
+                : null, //
+          ),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: RaisedButton(
+            color: Colors.redAccent,
+            child: const Text('Disconnect'),
+            onPressed: state == MQTTAppConnectionState.connected
+                ? _disconnect
+                : null, //
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSendButtonFrom(MQTTAppConnectionState state) {
+    return RaisedButton(
+      color: Colors.green,
+      child: const Text('Send'),
+      onPressed: state == MQTTAppConnectionState.connected
+          ? () {
+              _publishMessage(_messageTextController.text);
+            }
+          : null, //
+    );
+  }
+
+  Widget _builControllButtonFrom(MQTTAppConnectionState state) {
+    return RaisedButton(
+      color: Colors.green,
+      child: const Text('Controll'),
+      onPressed: state == MQTTAppConnectionState.connected
+          ? () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChangeNotifierProvider<MQTTAppState>(
+                      builder: (_) => MQTTAppState(),
+                      child: Control(),
+                    ),
+                    // Pass the arguments as part of the RouteSettings. The
+                    // DetailScreen reads the arguments from these settings.
+                    settings: RouteSettings(
+                      arguments: Params(currentAppState, manager),
+                    ),
+                  ));
+            }
+          : null, //
+    );
+  }
+
+  // Utility functions
+  String _prepareStateMessageFrom(MQTTAppConnectionState state) {
+    switch (state) {
+      case MQTTAppConnectionState.connected:
+        return 'Connected';
+      case MQTTAppConnectionState.connecting:
+        return 'Connecting';
+      case MQTTAppConnectionState.disconnected:
+        return 'Disconnected';
+    }
+    return " ";
+  }
+
+  void _configureAndConnect() {
+    // TODO: Use UUID
+    String osPrefix = 'Flutter_iOS';
+    if (Platform.isAndroid) {
+      osPrefix = 'Flutter_Android';
+    }
+    manager = MQTTManager(
+        host: _hostTextController.text,
+        topic: _topicTextController.text,
+        user: _userTextController.text,
+        password: _passwordTextController.text,
+        //  port:int.parse(_portTextController.text),
+        identifier: osPrefix,
+        state: currentAppState);
+    manager.initializeMQTTClient();
+    manager.connect();
+  }
+
+  void _disconnect() {
+    manager.disconnect();
+  }
+
+  void _publishMessage(String text) {
+    String osPrefix = 'Flutter_iOS';
+    if (Platform.isAndroid) {
+      osPrefix = 'Flutter_Android';
+    }
+    final String message = osPrefix + ' says: ' + text;
+    manager.publish(message);
+    _messageTextController.clear();
+  }
+}
---
+++ lib/mqtt/MQTTManager.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:roboremote/mqtt/state/MQTTAppState.dart';
+
+class MQTTManager {
+  // Private instance of client
+  final MQTTAppState _currentState;
+  MqttClient _client;
+  final String _identifier;
+  final String _host;
+  final String _topic;
+  final String _user;
+  final String _password;
+
+//final int _port;
+  // Constructor
+  MQTTManager({
+    @required String host,
+    @required String topic,
+    @required String identifier,
+    @required MQTTAppState state,
+    String user = null,
+    String password = null,
+    //int port
+  })  : _identifier = identifier,
+        _host = host,
+        _topic = topic,
+        _user = user,
+        _password = password,
+        _currentState = state;
+
+  void initializeMQTTClient() {
+    _client = MqttServerClient(_host, _identifier);
+    _client.keepAlivePeriod = 20;
+    //_client.checkCredentials(_user, _password);
+    _client.onDisconnected = onDisconnected;
+    _client.logging(on: true);
+    // _client.port=_port;
+    /// Add the successful connection callback
+    _client.onConnected = onConnected;
+    _client.onSubscribed = onSubscribed;
+
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier(_identifier)
+        .withWillTopic(
+            'willtopic') // If you set this you must set a will message
+        .withWillMessage('My Will message')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atLeastOnce);
+    print('EXAMPLE::Mosquitto client connecting....');
+    _client.connectionMessage = connMess;
+  }
+
+  MQTTAppState getState() {
+    return _currentState;
+  }
+
+  // Connect to the host
+  void connect() async {
+    assert(_client != null);
+    try {
+      print('EXAMPLE::Mosquitto start client connecting....');
+      _currentState.setAppConnectionState(MQTTAppConnectionState.connecting);
+      if (_user != null && _password != null)
+        await _client.connect(_user, _password);
+      else
+        await _client.connect();
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      disconnect();
+    }
+  }
+
+  void disconnect() {
+    print('Disconnected');
+    _client.disconnect();
+  }
+
+  void publish(String message) {
+    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+    _client.publishMessage(_topic, MqttQos.exactlyOnce, builder.payload);
+  }
+
+  /// The subscribed callback
+  void onSubscribed(String topic) {
+    print('EXAMPLE::Subscription confirmed for topic $topic');
+  }
+
+  /// The unsolicited disconnect callback
+  void onDisconnected() {
+    print('EXAMPLE::OnDisconnected client callback - Client disconnection');
+    if (_client.connectionStatus.returnCode ==
+        MqttConnectReturnCode.solicited) {
+      print('EXAMPLE::OnDisconnected callback is solicited, this is correct');
+    }
+    _currentState.setAppConnectionState(MQTTAppConnectionState.disconnected);
+  }
+
+  /// The successful connect callback
+  void onConnected() {
+    _currentState.setAppConnectionState(MQTTAppConnectionState.connected);
+    print('EXAMPLE::Mosquitto client connected....');
+    _client.subscribe(_topic, MqttQos.atLeastOnce);
+    _client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload;
+      final String pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      _currentState.setReceivedText(pt);
+
+      /// The above may seem a little convoluted for users only interested in the
+      /// payload, some users however may be interested in the received publish message,
+      /// lets not constrain ourselves yet until the package has been in the wild
+      /// for a while.
+      /// The payload is a byte buffer, this will be specific to the topic
+      print(
+          'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+      print('');
+    });
+    print(
+        'EXAMPLE::OnConnected client callback - Client connection was sucessful');
+  }
+}
